76fe1d70e7a5bb6ddb96c55858bd73627de18ef0
Phillip Jackson <unisys12@gmail.com>
2019-03-26 12:53:28 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongo = require("mongodb");
+const MongoClient = mongo.MongoClient;
+const port = process.env.PORT || 3001;
+const ghosts = require("./routes/api/ghosts");
+const armor = require("./routes/api/armor");
+const weapons = require("./routes/api/weapons");
+const sparrows = require("./routes/api/sparrows");
+const ships = require("./routes/api/ships");
+const ornaments = require("./routes/api/ornaments");
+const emblems = require("./routes/api/emblems");
+
+// Middleware
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Destiny Armory Defined API!");
+});
+
+app.use("/api/ghosts/", ghosts);
+app.use("/api/armor/", armor);
+app.use("/api/weapons", weapons);
+app.use("/api/sparrows", sparrows);
+app.use("/api/ships", ships);
+app.use("/api/ornaments", ornaments);
+app.use("/api/emblems", emblems);
+
+app.listen(port, () =>
+  console.log(`Armory backdoor is now open on port ${port}`)
+);
---
+++ server/routes/api/weapons.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const items = await loadItemCollection();
+  res.send(
+    await items
+      .find(
+        {
+          itemCategoryHashes: 1
+        },
+        { projection: { displayProperties: 1 } }
+      )
+      .toArray()
+  );
+});
+
+router.get("/:id", async (req, res) => {
+  const id = JSON.parse(req.params.id);
+  const items = await loadItemCollection();
+  res.send(
+    await items
+      .find(
+        { $and: [{ itemCategoryHashes: 1 }, { _id: id }] },
+        { projection: { displayProperties: 1 } }
+      )
+      .toArray()
+  );
+});
+
+async function loadItemCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb://localhost:27017",
+    { useNewUrlParser: true }
+  );
+  return client.db("Local_Armory").collection("DestinyInventoryItemDefinition");
+}
+
+module.exports = router;
